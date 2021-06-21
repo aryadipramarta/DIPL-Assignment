a470589e2882c4851129945c52bb88e007a17056
aryadipramarta <aryadipramarta9@gmail.com>
2021-06-21 23:18:30 +0800

    update

--- db_psstore.sql
+++ db_psstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 05:00 AM
+-- Generation Time: Jun 21, 2021 at 05:17 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -59,7 +59,27 @@ INSERT INTO `tb_alamatpembeli` (`id_alamat`, `id_pembeli`, `jalan_alamat`, `kota
 (34, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
 (35, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
 (36, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
-(37, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali');
+(37, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(38, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(39, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(40, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(41, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(42, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(43, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(44, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(45, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(46, 15, 'Jalan Merpati', 'Kota Denpasar', 80238, 'Bali'),
+(47, 15, '', '', 0, ''),
+(48, 15, '', '', 0, ''),
+(49, 15, '', '', 0, ''),
+(50, 15, '', '', 0, ''),
+(51, 15, '', '', 0, ''),
+(52, 15, '', '', 0, ''),
+(53, 20, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(54, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(55, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(56, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali'),
+(57, 15, 'Jalan Siulan Gang Nusa Indah I no 11', 'Kota Denpasar', 80238, 'Bali');
 
 -- --------------------------------------------------------
 
@@ -82,8 +102,11 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_barang`, `merk`, `spesifikasi`, `kondisi_barang`, `harga`, `stok`, `img_barang`) VALUES
-(1, 'Iphone 11 test', 'Layar Liquid Retina HD\r\nLayar LCD Multi-Touch sepenuhnya 6,1 inci (diagonal) dengan teknologi IPS\r\nResolusi 1792 x 828 piksel pada 326 ppi\r\nRasio kontras 1400:1 (umum)\r\nLayar True Tone\r\nLayar warna luas (P3)\r\nHaptic Touch\r\nKecerahan maks 625 nit (umu', 'Baru', 12500000, 12, 'mobile1.jpeg'),
-(2, 'Iphone 12', 'Rilis: 23 Oktober 2020.Berat: 164 g. Material: Kaca bagian depan Ceramic Shield, bagian belakang kaca, dan menggunakan bingkai aluminium. Jenis dan Ukuran Layar: Super Retina XDR OLED, HDR10, 6,1 inci (rasio layar ke bodi ~86%) Resolusi Layar: 1.170 ', 'Baru', 21500000, -4, 'iphone12.png');
+(1, 'Iphone 11', 'Layar Liquid Retina HD\r\nLayar LCD Multi-Touch sepenuhnya 6,1 inci (diagonal) dengan teknologi IPS\r\nResolusi 1792 x 828 piksel pada 326 ppi\r\nRasio kontras 1400:1 (umum)\r\nLayar True Tone\r\nLayar warna luas (P3)\r\nHaptic Touch\r\nKecerahan maks 625 nit (umu', 'Second', 12500000, 20, 'mobile1.jpeg'),
+(2, 'Iphone 12', 'Rilis: 23 Oktober 2020.Berat: 164 g. Material: Kaca bagian depan Ceramic Shield, bagian belakang kaca, dan menggunakan bingkai aluminium. Jenis dan Ukuran Layar: Super Retina XDR OLED, HDR10, 6,1 inci (rasio layar ke bodi ~86%) Resolusi Layar: 1.170 ', 'Second', 21500000, 9, 'iphone12.png'),
+(6, 'Samsung Galaxy Z Fold2 ', 'Samsung Galaxy Z Fold2, smartphone yang menawarkan layar lipat Dynamic AMOLED 2X 7,6 inci yang memiliki resolusi 1768 x 2208 pixels dan refresh rate 120Hz, serta menawarkan kamera belakang 12MP serta Snapdragon 865+ SoC.', 'New', 24590000, 8, 'no2.jpg'),
+(7, 'Iphone XR ', 'iPhone XR berlayar Liquid Retina LCD 6.1\", berbodi bahan alumunium, IP67, tidak dibekali 3D Touch, dibenami chip A12 Bionic dipadukan RAM 3GB, kamera belakang 12MP, kamera depan 7MP dengan TrueDepth Camera.', 'Second', 5390000, 15, 'xr.png'),
+(8, 'Samsung Galaxy Note20 Ultra', 'Samsung Galaxy Note20 Ultra ini memiliki layar 6.9 inci dengan resolusi QHD 1440 x 3088 pixels, Triple kamera 108MP, RAM 8GB dengan Exynos 990 dan 4.500 mAh.', 'Second', 9850000, 10, '20.jpg');
 
 -- --------------------------------------------------------
 
@@ -107,20 +130,6 @@ INSERT INTO `tb_kurir` (`id_Agent_Kurir`, `Nama_Agent`, `Ongkos_kirim`) VALUES
 (3, 'SiCepat', 23000),
 (4, 'Tiki', 28000),
 (5, 'Pos Indonesia', 35000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_laporan`
---
-
-CREATE TABLE `tb_laporan` (
-  `id_laporan` int(10) NOT NULL,
-  `id_pegawai` int(10) NOT NULL,
-  `id_transaksi` int(10) NOT NULL,
-  `judul_laporan` varchar(100) NOT NULL,
-  `tanggal_laporan` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -164,15 +173,32 @@ CREATE TABLE `tb_order` (
 --
 
 INSERT INTO `tb_order` (`id_order`, `id_pembeli`, `order_status`, `tanggal_beli`, `total_bayar`) VALUES
-(4, 19, 'Unconfirmed', '2021-06-01', 0),
-(5, 19, 'Unconfirmed', '2021-06-01', 0),
-(6, 19, 'Unconfirmed', '2021-06-01', 0),
-(7, 19, 'Unconfirmed', '2021-06-01', 55523000),
-(8, 15, 'Unconfirmed', '2021-06-02', 25025000),
-(9, 15, 'Unconfirmed', '2021-06-02', 25025000),
-(10, 15, 'Unconfirmed', '2021-06-02', 25025000),
+(7, 19, 'Confirmed', '2021-06-01', 55523000),
+(8, 15, 'Confirmed', '2021-06-02', 25025000),
+(9, 15, 'Confirmed', '2021-06-02', 25025000),
+(10, 15, 'Confirmed', '2021-06-02', 25025000),
 (11, 15, 'Unconfirmed', '2021-06-02', 34035000),
-(12, 15, 'Unconfirmed', '2021-06-02', 64522500);
+(12, 15, 'Unconfirmed', '2021-06-02', 64522500),
+(13, 15, 'Unconfirmed', '2021-06-02', 21522500),
+(14, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(15, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(16, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(17, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(18, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(19, 15, 'Unconfirmed', '2021-06-03', 21523000),
+(20, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(21, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(22, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(23, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(24, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(25, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(26, 15, 'Unconfirmed', '2021-06-03', 21525000),
+(27, 15, 'Confirmed', '2021-06-03', 21525000),
+(28, 20, 'Confirmed', '2021-06-04', 34035000),
+(29, 15, 'Unconfirmed', '2021-06-13', 24613000),
+(30, 15, 'Unconfirmed', '2021-06-15', 21525000),
+(31, 15, 'Unconfirmed', '2021-06-15', 21525000),
+(32, 15, 'Unconfirmed', '2021-06-21', 24612500);
 
 -- --------------------------------------------------------
 
@@ -192,8 +218,6 @@ CREATE TABLE `tb_order_detail` (
 --
 
 INSERT INTO `tb_order_detail` (`id_order_detail`, `id_barang`, `id_order`, `qty`) VALUES
-(5, 2, 4, 2),
-(6, 1, 4, 1),
 (7, 2, 7, 2),
 (8, 1, 7, 1),
 (9, 1, 8, 2),
@@ -201,7 +225,28 @@ INSERT INTO `tb_order_detail` (`id_order_detail`, `id_barang`, `id_order`, `qty`
 (11, 1, 10, 2),
 (12, 1, 11, 1),
 (13, 2, 11, 1),
-(14, 2, 12, 3);
+(14, 2, 12, 3),
+(15, 2, 13, 1),
+(16, 2, 14, 1),
+(17, 2, 15, 1),
+(18, 2, 16, 1),
+(19, 2, 17, 1),
+(20, 2, 18, 1),
+(21, 2, 19, 1),
+(22, 2, 20, 1),
+(23, 2, 21, 1),
+(24, 2, 22, 1),
+(25, 2, 23, 1),
+(26, 2, 24, 1),
+(27, 2, 25, 1),
+(28, 2, 26, 1),
+(29, 2, 27, 1),
+(30, 1, 28, 1),
+(31, 2, 28, 1),
+(32, 6, 29, 1),
+(33, 2, 30, 1),
+(34, 2, 31, 1),
+(35, 6, 32, 1);
 
 -- --------------------------------------------------------
 
@@ -253,11 +298,13 @@ INSERT INTO `tb_pembeli` (`id_pembeli`, `username_pembeli`, `password_pembeli`, 
 (12, 'andi123', '$2y$10$0hiW9o53iCE9EeQO0HQ0NOUy6bGJgmxVPH0/1IwJIPLxO82Hj7mG2', 'Andi Sentosa', '0812345671', 'andisentosa@gmail.com', 1),
 (13, 'delva123', '$2y$10$XLFTDwn2WbW2HOq/QmXoT.cTygFwWxRfw/G71V7cwu3/CyiZomhpC', 'Delvanita', '08123123122', 'delvanita@gmail.com', 1),
 (14, 'tes111', '$2y$10$lZUwnaWIwRqbDoH3enRBJeyrJOY80CMBjkfnqWWME8K9Q7sJwPKPy', 'Tes123', '08123123', 'asdasd@gmail.com', 1),
-(15, 'pembeli123', '$2y$10$ooPQ2.s0wC24kzOgxEHtw.UxuyDsrWcwqC72uGK5RbNUoDk9mmv/y', 'pembeli', '1234567890', 'pembeli123@gmail.com', 1),
+(15, 'pembeli123', '$2y$10$ooPQ2.s0wC24kzOgxEHtw.UxuyDsrWcwqC72uGK5RbNUoDk9mmv/y', 'pembeli', '0812312333', 'pembeli123@gmail.com', 1),
 (16, 'aryadi69', '$2y$10$a7jT7IOXjpb02mHRTgBTmO9S3PauZltw7UfIb0VnQ4VenUY1gfjEe', 'aryadi69', '081231123123', 'aryadi69@gmail.com', 1),
 (17, 'tes12345', '$2y$10$4RlR8i9MaYr6devJ6pf5n.U7/27DMANHCtg4YWrj/UKyvzQijpkwq', 'Testing123', '12312312', 'testing123@gmail.com', 1),
 (18, 'pembeli3', '$2y$10$deFstsHmhZlrpZWiGTADLeScAp2je3GDwhE9rAhdbmO93XxBElwTS', 'pembeli3', '081333333', 'pembeli333@gmail.com', 1),
-(19, 'abi1', '$2y$10$QSz6zSY5BM9Nn/NFXh1Oh.EMcHOQLMso3.bKR9uFoPBGb9lnNOEn.', 'abi', '001293123', 'abikadek@gmail.com', 1);
+(19, 'abi1', '$2y$10$QSz6zSY5BM9Nn/NFXh1Oh.EMcHOQLMso3.bKR9uFoPBGb9lnNOEn.', 'abi', '001293123', 'abikadek@gmail.com', 1),
+(20, 'pembelibaru', '$2y$10$91exrMxw1zlLEI37fYHk6eK2vPB7yuqUdVgzmrFPxflD5EJx56UAO', 'pembelibaru', '0812312312', 'pembelibaru123@gmail.com', 1),
+(21, 'jesica123', '$2y$10$Yp5dT6M23cNg/VhbLbu5H.pqyRrEXfK5wqo7gpwbzDN4Q5PJuo7yS', 'Jesica', '081231232', 'aryadipramarta22@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -303,7 +350,27 @@ INSERT INTO `tb_transaksi` (`id_transaksi`, `id_pembeli`, `id_Agent_Kurir`, `id_
 (2, 15, 1, 3, 25025000),
 (4, 15, 1, 3, 25025000),
 (5, 15, 5, 3, 34035000),
-(6, 15, 2, 4, 64522500);
+(6, 15, 2, 4, 64522500),
+(7, 15, 2, 3, 21522500),
+(8, 15, 3, 2, 21523000),
+(9, 15, 3, 2, 21523000),
+(10, 15, 3, 2, 21523000),
+(11, 15, 3, 2, 21523000),
+(12, 15, 3, 2, 21523000),
+(13, 15, 3, 2, 21523000),
+(14, 15, 3, 2, 21525000),
+(15, 15, 3, 2, 21525000),
+(16, 15, 1, 1, 21525000),
+(17, 15, 1, 1, 21525000),
+(18, 15, 1, 1, 21525000),
+(19, 15, 1, 1, 21525000),
+(20, 15, 1, 1, 21525000),
+(21, 15, 1, 1, 21525000),
+(22, 20, 5, 4, 34035000),
+(23, 15, 3, 2, 24613000),
+(24, 15, 1, 2, 21525000),
+(25, 15, 1, 3, 21525000),
+(26, 15, 2, 2, 24612500);
 
 --
 -- Indexes for dumped tables
@@ -327,14 +394,6 @@ ALTER TABLE `tb_barang`
 --
 ALTER TABLE `tb_kurir`
   ADD PRIMARY KEY (`id_Agent_Kurir`);
-
---
--- Indexes for table `tb_laporan`
---
-ALTER TABLE `tb_laporan`
-  ADD PRIMARY KEY (`id_laporan`),
-  ADD KEY `id_transaksi` (`id_transaksi`),
-  ADD KEY `tb_laporan ibfk_1` (`id_pegawai`);
 
 --
 -- Indexes for table `tb_metodepembayaran`
@@ -392,25 +451,19 @@ ALTER TABLE `tb_transaksi`
 -- AUTO_INCREMENT for table `tb_alamatpembeli`
 --
 ALTER TABLE `tb_alamatpembeli`
-  MODIFY `id_alamat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_alamat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tb_kurir`
 --
 ALTER TABLE `tb_kurir`
   MODIFY `id_Agent_Kurir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `tb_laporan`
---
-ALTER TABLE `tb_laporan`
-  MODIFY `id_laporan` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_metodepembayaran`
@@ -422,13 +475,13 @@ ALTER TABLE `tb_metodepembayaran`
 -- AUTO_INCREMENT for table `tb_order`
 --
 ALTER TABLE `tb_order`
-  MODIFY `id_order` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_order` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `tb_order_detail`
 --
 ALTER TABLE `tb_order_detail`
-  MODIFY `id_order_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_order_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `tb_pegawai`
@@ -440,7 +493,7 @@ ALTER TABLE `tb_pegawai`
 -- AUTO_INCREMENT for table `tb_pembeli`
 --
 ALTER TABLE `tb_pembeli`
-  MODIFY `id_pembeli` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_pembeli` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `tb_pemilik`
@@ -452,7 +505,7 @@ ALTER TABLE `tb_pemilik`
 -- AUTO_INCREMENT for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Constraints for dumped tables
@@ -463,13 +516,6 @@ ALTER TABLE `tb_transaksi`
 --
 ALTER TABLE `tb_alamatpembeli`
   ADD CONSTRAINT `fk_pembeli1` FOREIGN KEY (`id_pembeli`) REFERENCES `tb_pembeli` (`id_pembeli`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tb_laporan`
---
-ALTER TABLE `tb_laporan`
-  ADD CONSTRAINT `tb_laporan ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `tb_pegawai` (`id_pegawai`),
-  ADD CONSTRAINT `tb_laporan_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_transaksi` (`id_transaksi`);
 
 --
 -- Constraints for table `tb_order`
